2d2982e881ad6f11c4c3233763dd3b4599cb56b0
angryBunny <praga617@mail.ru>
2024-06-24 23:52:10 +0500

    Saving the original program

---
+++ src/main/java/pro/sky25/_CollectionsSheetsAndSets/bazhinaOA/Application.java
@@ -0,0 +1,13 @@
+package pro.sky25._CollectionsSheetsAndSets.bazhinaOA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
